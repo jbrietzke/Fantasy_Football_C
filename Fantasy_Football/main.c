99c5027d0acb9806a6b99fdbad767aac1bb797bc
Jackson Brietzke <jacksonbrietzke@Mecatol.local>
2017-07-05 20:24:40 -0400

    Initial Commit

---
+++ Fantasy_Football/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Fantasy_Football
+//
+//  Created by Jackson Brietzke on 7/5/17.
+//  Copyright © 2017 Jackson Brietzke. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
